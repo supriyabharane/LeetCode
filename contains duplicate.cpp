feacae8ea75b82f88bb760e9c75807040abf153f
supriya bharane <145009664+supriyabharane@users.noreply.github.com>
2024-07-14 12:47:53 +0530

    Add files via upload

---
+++ contains duplicate.cpp
@@ -0,0 +1,7 @@
+ sort(nums.begin(),nums.end());
+        bool flag = false;
+        for(int i =0;i<nums.size()-1;i++){
+            if(nums[i] == nums[i+1]) return true;
+        }
+        return flag;
+        
